39e106c4085eefd981de4302d7eaa2d9d4df68d2
Nithish <Nithishoffl24@gmail.com>
2025-04-03 21:31:07 +0530

    minor changes

--- demo/src/main/java/com/example/demo/DemospringApplication.java
+++ demo/src/main/java/com/example/demo/DemospringApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemospringApplication {
+	static Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemospringApplication.class, args);
+		HomeController hc = new HomeController();
+		int x = s.nextInt();
+		if (hc.sint == x) {
+			hc.sample2();
+		}
 	}
-
 }
--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,17 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${app.page-size}")
+    String name;
+    @Value("${sampleint}")
+    int sint;
+
     @RequestMapping("/")
-    public String sample2(){
+    public String sample2() {
+        System.out.println("sample2 called");
         return "fine.html";
     }
+
     @RequestMapping("/home")
     public String sample() {
-        System.out.println("hi");
+
+        System.out.println(name);
         return "index.html";
     }
 
